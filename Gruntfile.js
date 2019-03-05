d62cbb7855625e3796860969251b184da8a58522
lixiaofan1997 <773515092@qq.com>
2019-03-05 20:46:41 +0800

    htmlhint分支

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports=function(grunt){
+grunt.initConfig({
+    htmlhint: {
+      html: {
+        options: {
+          htmlhintrc: '.htmlhintrc'
+        },
+        src: ['*.html']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+  grunt.registerTask('default', ['htmlhint']);
+};
