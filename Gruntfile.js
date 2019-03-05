1b507cbea95b60c838f8175cd982c17e75680485
lixiaofan1997 <773515092@qq.com>
2019-03-05 21:52:12 +0800

    eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    csslint: {
+    eslint: {
       options: {
-        csslintrc: '.csslintrc'
+        configFile: '.eslintrc.json'
       },
-      src: ['rectangle.css']
+      target: ['rectangle.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');
 
-  grunt.registerTask('default', ['csslint']);
+  grunt.registerTask('default', ['eslint']);
 };
 
--- rectangle.js
+++ rectangle.js
@@ -4,14 +4,14 @@ $(function(){
       $btncal=$('#calculate'),
       $perimeter=$("#perimeter"),
       $area=$('#area');
-$btncal.click(function(){
-  var w=Number($width.val())
-      h=Number($height.val());
-  var p=2*(w+h),
-      a=w*h;
-  $perimeter.val(p);
-  $area.val(a);
+  $btncal.click(function(){
+    var w=Number($width.val()),
+        h=Number($height.val());
+    var p=2*(w+h),
+        a=w*h;
+    $perimeter.val(p);
+    $area.val(a);
 
-});
+  });
   
 });
