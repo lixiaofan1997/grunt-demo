1052271e98dc157eb69f75478f33ebd6c322ce48
lixiaofan1997 <773515092@qq.com>
2019-03-12 16:11:49 +0800

    sprite

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sprite: {
+        all: {
+           src: './images/*.png',
+           dest: './images/all.png',
+           destCss: './sprites.css'
+                                                                  
+      }
+
+    }
+              
+  });
+
+    grunt.loadNpmTasks('grunt-spritesmith');
+
+      grunt.registerTask('default', ['sprite']);
+
+};
