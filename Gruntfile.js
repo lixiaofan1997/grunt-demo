335a18b02c0c28f25788f9f6b612297976dfd346
lixiaofan1997 <773515092@qq.com>
2019-03-12 19:31:27 +0800

    cssmin  success

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
         cssmin: {  
               'rectangle.min.css': 'rectangle.css'
-            }
-        options: {
-          mergeIntoShorthands: false,
-          roundingPrecision: -1
-        },
-     target: {
-      files: {
-            'output.css': ['foo.css', 'bar.css']
-        
-      }
-             
-  }
-                  
+        }
   });
 
       grunt.loadNpmTasks('grunt-contrib-cssmin');
